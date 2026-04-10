c5213a42f836e111b517f165b5f288e633095834
Eri_G <132447977+moooeg@users.noreply.github.com>
2026-04-10 21:58:07 +1000

    add code in auto 2, change from auto 1, not tested

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -12,11 +12,14 @@ void SoloAutonomous() {
 }
 
 void Auto1() {
+    //get three center block
     IntakeStart();
     chassis.moveToPoint(-3, 17, 2000, {.minSpeed=72, .earlyExitRange=8});
     chassis.moveToPoint(-6, 45, 2000, {.maxSpeed=32});
     pros::delay(900);
     matchload.extend();
+
+    //go to match load tube
     chassis.turnToHeading(chassis.getPose().theta - 155, 1000, {.maxSpeed=100});
     pros::delay(500);
     IntakeStart(-127, true, false, true);
@@ -24,7 +27,7 @@ void Auto1() {
     pros::delay(100);
     IntakeStart(127, true, false, true);
     chassis.moveToPoint(-33, 0, 300, {.maxSpeed=64});
-    chassis.moveToPoint(-33, chassis.getPose().y - 10, 300, {.forwards=false});
+    chassis.moveToPoint(-33, chassis.getPose().y + 10, 300, {.forwards=false});
     chassis.moveToPoint(-33, 0, 300, {.maxSpeed=64});
     pros::delay(1500);
     chassis.setPose(0, chassis.getPose().y, chassis.getPose().theta); // set x to be 0
@@ -54,8 +57,38 @@ void Auto1() {
 }
 
 void Auto2() {
+    chassis.setPose(0, 0, 0);
+    // get three center block
+    chassis.moveToPoint(-3, 17, 2000, {.minSpeed = 72, .earlyExitRange = 8});
+    IntakeStart();
+    chassis.moveToPoint(-6, 45, 2000, {.maxSpeed = 32});
+    pros::delay(900);
+    matchload.extend();
 
-    std::cout << "Hello" << std::endl;
+    // go to match load tube
+    chassis.turnToHeading(315, 1000, {.maxSpeed = 100});
+    pros::delay(500);
+    chassis.moveToPose(-33, 20, 180, 1500, {.lead = 0.1, .maxSpeed = 100});
+    pros::delay(100);
+    chassis.moveToPoint(-33, 0, 300, {.maxSpeed = 64});
+    pros::delay(1500);
+
+    // score in long goal
+    chassis.moveToPoint(-33, 45, 1500, {.forwards = false}, false);
+    holder.retract();
+    matchload.retract();
+    pros::delay(1500);
+    IntakeStop();
+    chassis.moveToPoint(-33, 35, 500, {.forwards = false}, false);
+    holder.extend();
+    chassis.moveToPoint(-33, 45, 500, {.forwards = false}, false);
+    pros::delay(500);
+
+    //push to center
+    chassis.moveToPoint(-33, 25, 1000,{},false);
+    descore.retract();
+    chassis.moveToPose(-23, 45, 0, 1000, {.forwards = false, .lead = 0.8});
+    chassis.moveToPoint(-23, 60, 1000, {.forwards = false});
 }
 
 void Skills() {
