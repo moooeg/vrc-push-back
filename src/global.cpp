c38ead495ccff7757f009d6110eac1a86c929fe1
Eri_G <132447977+moooeg@users.noreply.github.com>
2026-04-14 11:54:02 +1000

    sensitivity

--- src/global.cpp
+++ src/global.cpp
@@ -104,14 +104,14 @@ lemlib::OdomSensors sensors(&vertical, // vertical tracking wheel
 
 // input curve for throttle input during driver control
 lemlib::ExpoDriveCurve throttleCurve(3, // joystick deadband out of 127
-                                     10, // minimum output where drivetrain will move out of 127
+                                     5, // minimum output where drivetrain will move out of 127
                                      1.019 // expo curve gain
 );
 
 // input curve for steer input during driver control
 lemlib::ExpoDriveCurve steerCurve(3, // joystick deadband out of 127
-                                  10, // minimum output where drivetrain will move out of 127
-                                  1.019 // expo curve gain
+                                  5, // minimum output where drivetrain will move out of 127
+                                  1.1 // expo curve gain
 );
 
 // create the chassis
